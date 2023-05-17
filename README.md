56f00fbadf87d188e33ab989d419e1d334a5e317
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2023-05-17 10:25:35 +0530

    updated script and README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # VIT-wifi-Auto-Login
+Wifi autologin scrpit written in python using selenium for Vellore Institute of Technology (VIT) <br>
+** You need to modify the script with your login credentials. Please open the script and follow the instructions **
+
+
--- vitwifi.py
+++ vitwifi.py
@@ -1,19 +1,37 @@
+import os
+import sys
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-a = input("Enter your regno.::")
-b = input("Enter your Password::")
+regnum = 'XXXXXXX' #Enter your registration number here
+pwsd = 'XXXXXX' #Enter your password here
+routername = "VIT5G"
 
-driver = webdriver.Chrome()
+
+
+# The following 2 lines are written for linux to autoconnect to the wifi.
+os.system(f"nmcli dev wifi connect {routername}")
+# For windows uncomment the line below (not tested)
+#os.system(f'''cmd /c "netsh wlan connect name={routername}"''')
+time.sleep(3)
+
+options = Options()
+options.binary_location = "/usr/bin/brave" # Modify this string with the path to your chrome executable. 
+options.add_argument("headless")
+driver = webdriver.Chrome(options=options)
+
 driver.get("http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnecttest.com/redirect")
+driver.implicitly_wait(3)
 
-username = driver.find_element_by_name("userId")
-password = driver.find_element_by_name("password")
-username.send_keys(a)
-password.send_keys(b)
-btn = driver.find_element_by_name("Submit22")
+username = driver.find_element(By.NAME, "userId")
+password = driver.find_element(By.NAME, "password")
+username.send_keys(regnum)
+driver.implicitly_wait(1)
+password.send_keys(pwsd)
+driver.implicitly_wait(1)
+btn = driver.find_element(By.NAME, "Submit22")
 btn.click()
 
-time.sleep(3)
-time.close()
+driver.implicitly_wait(3)
